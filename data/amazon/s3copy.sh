81a67de713e8aecfac4f40a062c683b22c9a1a13
danhphan <danh.phan.mq@gmail.com>
2021-10-09 15:13:52 +1100

    add s3copy.sh

---
+++ data/amazon/s3copy.sh
@@ -0,0 +1,8 @@
+aws s3 cp s3://amazon-reviews-pds/tsv/amazon_reviews_us_Books_v1_00.tsv.gz ./data/amazon
+aws s3 cp s3://amazon-reviews-pds/tsv/amazon_reviews_us_Digital_Ebook_Purchase_v1_00.tsv.gz ./data/amazon
+aws s3 cp s3://amazon-reviews-pds/tsv/amazon_reviews_us_Home_v1_00.tsv.gz ./data/amazon
+aws s3 cp s3://amazon-reviews-pds/tsv/amazon_reviews_us_Automotive_v1_00.tsv.gz ./data/amazon
+aws s3 cp s3://amazon-reviews-pds/tsv/amazon_reviews_multilingual_US_v1_00.tsv.gz ./data/amazon
+aws s3 cp s3://amazon-reviews-pds/tsv/amazon_reviews_us_Beauty_v1_00.tsv.gz ./data/amazon
+aws s3 cp s3://amazon-reviews-pds/tsv/amazon_reviews_us_Electronics_v1_00.tsv.gz ./data/amazon
+aws s3 cp s3://amazon-reviews-pds/tsv/amazon_reviews_us_Health_Personal_Care_v1_00.tsv.gz ./data/amazon
